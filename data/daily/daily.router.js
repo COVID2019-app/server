612bdb0c3618df5b293a1d12b3b0a3f5c4dee0c2
Mike Harley <mharley12345@gmail.com>
2020-03-13 06:03:58 -0700

    adds usa regions router and seed data

--- data/daily/daily.router.js
+++ data/daily/daily.router.js
@@ -2,6 +2,21 @@ const router = require("express").Router();
 const db = require("./daily.model")
 
 
-router.get('/',(req,res)=>{
-    
+router.post('/',(req,res)=>{
+    const data = req.body
+    db.postData(data)
+    .then(response =>{
+        res.status(201).json(response)
+    })
+    .catch(err =>res.status(401).json(err.message))
 })
+
+router.get('/',(req,res) =>{
+    db.getData()
+    .then(response =>{
+        res.status(200).json(response)
+    })
+    .catch(err => {res.status(401).json(err.message)})
+})
+
+module.exports = router
--- data/db.js
+++ data/db.js
@@ -3,8 +3,10 @@ const knex = require('knex');
 
 const knexfile = require('../knexfile');
 
+const DB_URL = process.env.DB_URL
+const HOME_URL = process.env.HOME_URL
 
-const env = process.env.NODE_ENV || 'development';
-const configOptions = knexfile[env];
+const envi = process.env.NODE_ENV || 'development';
+const configOptions = knexfile[envi];
 
 module.exports = knex(configOptions);
