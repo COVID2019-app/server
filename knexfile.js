360b054b5a181d0c2f8e3d331277073dc9a28f99
Mike Harley <mharley12345@gmail.com>
2020-03-16 10:07:26 -0700

    hooks staging up to test database

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@
     development: {
     client:'pg',
   
-      connection:process.env.DB_URL,
+      connection:'postgres://kkirxdknqfudhr:3c918d64ec42fe1661df070141f8e30d3991ce04a4ebe82b8acaaab04bb3669c@ec2-18-209-187-54.compute-1.amazonaws.com:5432/d9i8nr6ktbvgi3?ssl=true',
       rejectUnauthorized:false
       
 
@@ -23,7 +23,7 @@
       production: {
         client:'pg',
       
-          connection:process.env.DB_URL,
+          connection:'postgres://kkirxdknqfudhr:3c918d64ec42fe1661df070141f8e30d3991ce04a4ebe82b8acaaab04bb3669c@ec2-18-209-187-54.compute-1.amazonaws.com:5432/d9i8nr6ktbvgi3?ssl=true',
           
     
      
