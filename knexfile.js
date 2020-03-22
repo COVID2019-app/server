0681fce9c477c2d0ec974b1bfabe34c8712d34aa
Min-Huang <minh.huang96@gmail.com>
2020-03-22 13:19:26 -0700

    changed env.dburl

--- knexfile.js
+++ knexfile.js
@@ -2,17 +2,17 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: process.env.DB_URL,
-    rejectUnauthorized: false
+    connection: process.env.DATABASE_URL,
+    rejectUnauthorized: false,
   },
   production: {
     client: 'pg',
-    connection: process.env.DB_URL
+    connection: process.env.DATABASE_URL,
   },
   migrations: {
-    directory: './migrations'
+    directory: './migrations',
   },
   seeds: {
-    directory: './seeds'
-  }
+    directory: './seeds',
+  },
 };
